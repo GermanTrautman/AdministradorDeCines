da905b9e22c2ef4a9234e365b5e55df2a238430a
Andres Cardozo <acardozoreimer@gmail.com>
2018-10-17 20:26:50 -0300

    Cambia el alta de cliente por el de usuario

--- src/main/java/com/cine/controlador/ControladorUsuario.java
+++ src/main/java/com/cine/controlador/ControladorUsuario.java
@@ -1,16 +1,16 @@
 package com.cine.controlador;
 
-import com.cine.modelo.Cliente;
+import com.cine.modelo.Usuario;
 
 public class ControladorUsuario {
 
 	public void altaUsuario(String nombreDeUsuario, String email, String password, String nombre, String domicilio, String dni, String fechaDeNacimiento) {
 		
-		Cliente cliente = new Cliente();
-		cliente.setNombreDeUsuario(nombreDeUsuario);
-		cliente.setEmail(email);
-		cliente.setPassword(password);
-		cliente.setNombre(nombre);
-		cliente.setDomicilio(domicilio);
+		Usuario usuario = new Usuario();
+		usuario.setNombreDeUsuario(nombreDeUsuario);
+		usuario.setEmail(email);
+		usuario.setPassword(password);
+		usuario.setNombre(nombre);
+		usuario.setDomicilio(domicilio);
 	}
 }
