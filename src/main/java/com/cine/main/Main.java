2551b3d2088f73d45e6f6f02061517a642c6fa66
GermanTrautman <germantrautman@gmail.com>
2018-10-18 01:28:29 -0300

    cambios menores

--- src/main/java/com/cine/main/Main.java
+++ src/main/java/com/cine/main/Main.java
@@ -4,7 +4,6 @@ import com.cine.modelo.Usuario;
 import com.cine.vista.Principal;
 
 import java.awt.*;
-import java.sql.ResultSet;
 
 public class Main {
 
@@ -15,8 +14,6 @@ public class Main {
                     Usuario usr = new Usuario();
                     Principal window = new Principal();
                     window.framePrincipal.setVisible(true);
-                    Usuario usuario = new Usuario();
-                    ResultSet lala = usuario.buscarUsuario();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
