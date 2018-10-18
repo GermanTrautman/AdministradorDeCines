2c81bcac4ccf4cd533ad9dc80e36a7b7f45fb722
GermanTrautman <germantrautman@gmail.com>
2018-10-18 01:23:54 -0300

    Configuracion de la db, sqlDriver jar agregado, mejoras en UI

--- src/main/java/com/cine/main/Main.java
+++ src/main/java/com/cine/main/Main.java
@@ -1,8 +1,26 @@
 package com.cine.main;
 
+import com.cine.modelo.Usuario;
+import com.cine.vista.Principal;
+
+import java.awt.*;
+import java.sql.ResultSet;
+
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Usuario usr = new Usuario();
+                    Principal window = new Principal();
+                    window.framePrincipal.setVisible(true);
+                    Usuario usuario = new Usuario();
+                    ResultSet lala = usuario.buscarUsuario();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
