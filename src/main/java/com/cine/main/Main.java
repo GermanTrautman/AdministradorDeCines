c4db5dde560743a68fe2859e3ca2f7d415b7160d
Andres Cardozo <acardozoreimer@gmail.com>
2018-10-18 23:40:49 -0300

    Elimina la instancia del usuario

--- src/main/java/com/cine/main/Main.java
+++ src/main/java/com/cine/main/Main.java
@@ -1,30 +1,21 @@
 package com.cine.main;
 
-import com.cine.modelo.Usuario;
 import com.cine.vista.Principal;
 
 import java.awt.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Principal window = new Principal();
-                    window.framePrincipal.setVisible(true);
-                    Usuario usr = new Usuario();
-
-                    usr.setNombreDeUsuario("Macri");
-                    usr.setNombre("Gato");
-                    usr.setDni(23453432);
-                    usr.insertarUsuario(usr);
-
-                    usr.buscarUsuario(39459185);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal window = new Principal();
+					window.framePrincipal.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
