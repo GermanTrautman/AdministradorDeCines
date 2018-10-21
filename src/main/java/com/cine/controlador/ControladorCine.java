70db78c90c21bcd36cf6b2a351a61ce738719b57
Andres Cardozo <acardozoreimer@gmail.com>
2018-10-21 13:07:48 -0300

    Define la clase como un singleton

--- src/main/java/com/cine/controlador/ControladorCine.java
+++ src/main/java/com/cine/controlador/ControladorCine.java
@@ -1,13 +1,28 @@
 package com.cine.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cine.Dao.EstablecimientoPersistente;
 import com.cine.modelo.Establecimiento;
 
 public class ControladorCine {
+	
+	private static final ControladorCine controladorCine = new ControladorCine();
+
+    private ControladorCine() {}
+
+    public static ControladorCine getInstance() {
+    	return controladorCine; 
+    }
+	
+	private List<Establecimiento> establecimientos = new ArrayList<Establecimiento>();
 
 	public void altaEstablecimiento(Integer cuit, String nombre, String domicilio, Integer capacidad) {
 
 		Establecimiento establecimiento = new Establecimiento(cuit, nombre, domicilio, capacidad);
 		
-		establecimiento.guardar(establecimiento);
+		EstablecimientoPersistente establecimientoPersistente = new EstablecimientoPersistente();
+		establecimientoPersistente.insertar(establecimientos, establecimiento);
 	}
 }
