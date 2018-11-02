cfaa538da21bb646989f7f616b9c781777c4948c
Ger <germantrautman@gmail.com>
2018-11-02 02:01:28 -0300

    usuario/roles terminado

--- src/main/java/com/cine/modelo/AgenteComercial.java
+++ src/main/java/com/cine/modelo/AgenteComercial.java
@@ -1,9 +1,6 @@
 package com.cine.modelo;
 
-import com.cine.utilidades.Estado;
-
 public class AgenteComercial extends Rol {
-    public AgenteComercial(String nombre, Estado estado) {
-        super(nombre, estado);
+    public AgenteComercial() {
     }
 }
---
+++ src/main/java/com/cine/modelo/RolUsuario.java
@@ -0,0 +1,54 @@
+package com.cine.modelo;
+
+import com.cine.dao.RolUsuarioPersistente;
+
+public class RolUsuario {
+
+    private Integer idUsuario;
+    private Integer idRol;
+
+    public RolUsuario(Integer idUsuario, Integer idRol) {
+        this.idUsuario = idUsuario;
+        this.idRol = idRol;
+    }
+
+    public RolUsuario() {
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Integer getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+
+    public void insertarRolUsuario() {
+        RolUsuarioPersistente.getInstance().insertar(this);
+    }
+
+    public Integer buscarRolPorNombre(String nombre){
+        return RolUsuarioPersistente.getInstance().buscarPorNombreRol(nombre);
+    }
+
+    public void borrarRolUsuario() {
+        RolUsuarioPersistente.getInstance().borrar(this.getIdUsuario());
+    }
+
+    public void actualizarRolUsuario(RolUsuario rolUsuario) {
+        RolUsuarioPersistente.getInstance().actualizar(rolUsuario);
+    }
+
+    public RolUsuario buscarRolUsuario(Integer dni) {
+        return RolUsuarioPersistente.getInstance().buscar(dni);
+    }
+}
