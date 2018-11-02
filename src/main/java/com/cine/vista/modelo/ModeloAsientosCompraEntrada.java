036f4ef6d7eaff45e8fa03b9f0041805e7811b0d
Andres Cardozo <acardozoreimer@gmail.com>
2018-11-02 01:04:22 -0300

    Agrega la pantalla de compra de entradas

---
+++ src/main/java/com/cine/vista/modelo/ModeloAsientosCompraEntrada.java
@@ -0,0 +1,26 @@
+package com.cine.vista.modelo;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ModeloAsientosCompraEntrada extends AbstractTableModel {
+
+	private static final long serialVersionUID = -6792306576651076683L;
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 25;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 25;
+	}
+
+	@Override
+	public Object getValueAt(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
