95bcdef3fe73c076dd8900fdbb903e28db7308c3
Andres Cardozo <acardozoreimer@gmail.com>
2018-11-08 21:23:34 -0300

    Agrega ver asientos

---
+++ src/main/java/com/cine/vista/JFormularioVerAsientos.java
@@ -0,0 +1,113 @@
+package com.cine.vista;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import javax.swing.SwingConstants;
+
+import com.cine.controlador.ControladorSala;
+import com.cine.modelo.AsientoFisico;
+import com.cine.modelo.Sala;
+import com.cine.utilidades.Estado;
+
+public class JFormularioVerAsientos extends JFormularioBase {
+
+	private static final long serialVersionUID = -2608943734882268555L;
+	
+	private AsientoFisico[][] asientos = new AsientoFisico[100][100];
+	
+	private JPanel panelDeAsientos = new JPanel();
+	
+	private JButton btnGuardar = new JButton();
+	
+	private Integer cantidadDeFilas = 0;
+	private Integer cantidadDeAsientosPorFila = 0;
+
+	public JFormularioVerAsientos(String nombreDeSala) {
+		
+		Sala sala = ControladorSala.getInstance().buscar(nombreDeSala);
+
+		getContentPane().setLayout(null);
+		
+		JLabel lblAltaAsientos = new JLabel("Asientos");
+		lblAltaAsientos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAltaAsientos.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblAltaAsientos.setBounds(60, 30, 1024, 38);
+		this.getContentPane().add(lblAltaAsientos);
+		
+		panelDeAsientos.setLayout(new GridLayout(cantidadDeFilas, cantidadDeAsientosPorFila));
+		panelDeAsientos.setBounds(100, 100, 1000, 700);
+		
+		ActionListener actionListener = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			
+				JToggleButton boton = (JToggleButton) e.getSource();
+				String filaYAsiento = boton.getName();
+				
+				agregarOQuitarSala(nombreDeSala, filaYAsiento);
+			}
+		};
+
+		for (int i = 0; i < cantidadDeFilas; i++) {
+
+			for (int j = 0; j < cantidadDeAsientosPorFila; j++) {
+
+				Integer numeroFila = i + 1;
+				Integer numeroAsientoPorFila = j + 1;
+
+				JToggleButton boton = new JToggleButton("F: " + numeroFila + " A: " + numeroAsientoPorFila);
+				boton.setName(numeroFila.toString() + "," + numeroAsientoPorFila.toString());
+				boton.addActionListener(actionListener);
+				panelDeAsientos.add(boton);
+			}
+		}
+		
+		btnGuardar.setText("Guardar");
+		btnGuardar.setBounds(550, 870, 115, 29);
+		btnGuardar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				ControladorSala.getInstance().modificarAsientos(nombreDeSala, asientos);
+				
+				JOptionPane.showMessageDialog(null, "Se han guardado los asientos");
+				
+				dispose();
+			}
+		});
+		
+		getContentPane().add(panelDeAsientos);
+		getContentPane().add(btnGuardar);
+	}
+
+	private void agregarOQuitarSala(String nombreDeSala, String filaYAsiento) {
+		
+		String[] filaYAsientoPartido = filaYAsiento.split(Pattern.quote(","));
+		Integer fila = Integer.parseInt(filaYAsientoPartido[0]);
+		Integer numeroDeAsiento = Integer.parseInt(filaYAsientoPartido[1]);
+		
+		AsientoFisico asientoFisico = new AsientoFisico(nombreDeSala, fila, numeroDeAsiento, Estado.ACTIVO);
+		
+		/*TODO
+		boolean seAgregoElAsiento = asientos.add(asientoFisico);
+		
+		if (!seAgregoElAsiento) {
+			asientos.remove(asientoFisico);
+		}
+		*/
+	}
+}
